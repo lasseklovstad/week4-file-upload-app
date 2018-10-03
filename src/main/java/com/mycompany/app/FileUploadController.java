d86b7c29cbff863add0c7bb62788084c3078aa3f
Lasse.Klovstad <lasse.klovstad@gmail.com>
2018-10-03 14:17:01 +0200

    fileuploader

--- src/main/java/com/mycompany/app/FileUploadController.java
+++ src/main/java/com/mycompany/app/FileUploadController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 
@@ -22,9 +23,9 @@ public class FileUploadController {
         return "upload";
     }
     @RequestMapping(value = "/display",method= RequestMethod.POST)
-    public String display(Model model){
-
-       
+    public String display(@RequestParam("file") MultipartFile file){
+	
+       System.out.println(file.getOriginalFilename());
         return "display";
     }
 }
